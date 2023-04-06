a76126eff07c1d1bd7bb09660f0371126dc810b2
AndreiChistikhin <78159702+AndreiChistikhin@users.noreply.github.com>
2023-04-06 22:08:56 +0700

    Добавлен Product в базу данных, есть возможность создавать, удалять и редактировать нужный Product

---
+++ WebSite/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebSite.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(1,int.MaxValue)]
+        public string Price { get; set; }
+        public string Image { get; set; }
+
+        [Display(Name = "Category Type")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CaregoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
